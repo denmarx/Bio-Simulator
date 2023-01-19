1fe8dee83fff391aa7195eea5fa2f0547729dc49
undeadgorilla <dennis.marx93@gmail.com>
2023-01-19 18:08:17 +0100

    initial commit

--- biosimulator/src/components/App.js
+++ biosimulator/src/components/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Slider from '../components/Slider';
 import Water from '../components/Water';
 import Ph from '../components/Ph';
+import Scene from './Scene';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -32,38 +33,41 @@ export default class App extends React.Component {
 
   render() {
     return (
-      <div className="bosstainer">
-        <div className="uiContainer">
-          <button
-            className="createEnzymeButton"
-            onClick={this.enzymeBtnClicked}
-          >
-            Create Enzyme
-          </button>
-          <Slider
-            name="tempSlider"
-            min={-51}
-            max={51}
-            startValue={20}
-            changeHandler={this.handleWaterSlider}
-            desc="Temperature"
+      <div>
+        <Scene />
+        <div className="bosstainer">
+          <div className="uiContainer">
+            <button
+              className="createEnzymeButton"
+              onClick={this.enzymeBtnClicked}
+            >
+              Create Enzyme
+            </button>
+            <Slider
+              name="tempSlider"
+              min={-51}
+              max={51}
+              startValue={20}
+              changeHandler={this.handleWaterSlider}
+              desc="Temperature"
+            />
+            <Slider
+              name="phSlider"
+              min={-1}
+              max={15}
+              startValue={7}
+              changeHandler={this.handlePhSlider}
+              desc="pH"
+            />
+          </div>
+          <Water
+            title="Temperature: "
+            startTemp={20}
+            ref={this.waterRef}
+            unit="°C"
           />
-          <Slider
-            name="phSlider"
-            min={-1}
-            max={15}
-            startValue={7}
-            changeHandler={this.handlePhSlider}
-            desc="pH"
-          />
+          <Ph title="pH: " startPh={7} ref={this.phRef} />
         </div>
-        <Water
-          title="Temperature: "
-          startTemp={20}
-          ref={this.waterRef}
-          unit="°C"
-        />
-        <Ph title="pH: " startPh={7} ref={this.phRef} />
       </div>
     );
   }
