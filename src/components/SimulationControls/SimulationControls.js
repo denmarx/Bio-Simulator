915484a42096e49c56fd137ef7015461664543e5
undeadgorilla <dennis.marx93@gmail.com>
2024-01-16 13:58:07 +0100

    button refactored complete

--- src/components/SimulationControls/SimulationControls.js
+++ src/components/SimulationControls/SimulationControls.js
@@ -7,25 +7,7 @@ const SimulationControls = ({ temp, ph, onTempChange, onPhChange, onNutrientAdd,
     <div className='controls-container'>
       <Slider label='Temperature' min={-5} max={37} value={temp} onChange={onTempChange} />
       <Slider label='pH' min={-1} max={15} value={ph} onChange={onPhChange} />
-      <Buttons onNutrientAdd={onNutrientAdd} />
-      {/* <button className='button' onClick={() => onNutrientAdd('carbohydrates')}>
-        Add Carbohydrates
-      </button>
-      <button className='button' onClick={() => onNutrientAdd('proteins')}>
-        Add Proteins
-      </button>
-      <button className='button' onClick={() => onNutrientAdd('lipids')}>
-        Add Lipids
-      </button>
-      <button className='button' onClick={() => onEnzymeAdd('amylase', "carbohydrates")}>
-        Create Amylase
-      </button>
-      <button className='button' onClick={() => onEnzymeAdd('protease', "proteins")}>
-        Create Protease
-      </button>
-      <button className='button' onClick={() => onEnzymeAdd('lipase', "lipids")}>
-        Create Lipase
-      </button> */}
+      <Buttons onNutrientAdd={onNutrientAdd} onEnzymeAdd={onEnzymeAdd} />
     </div>
   );
 };
--- src/components/Slider/Buttons.js
+++ src/components/Slider/Buttons.js
@@ -1,17 +1,26 @@
 import React from 'react';
 
 // Slider is arrow function which takes one object with properties
-const Buttons = ({ onNutrientAdd }) => {
+const Buttons = ({ onNutrientAdd, onEnzymeAdd }) => {
   return (
     <>
       <button id='carbohydrates' onClick={() => onNutrientAdd('carbohydrates')}>
         Add Carbohydrates
       </button>
       <button id='proteins' className='button' onClick={() => onNutrientAdd('proteins')}>
-        Add Carbohydrates
+        Add Proteins
       </button>
       <button id='lipids' className='button' onClick={() => onNutrientAdd('lipids')}>
-        Add Carbohydrates
+        Add Lipids
+      </button>
+      <button id='amylase' className='button' onClick={() => onEnzymeAdd('amylase', 'carbohydrates')}>
+        Add Amylase
+      </button>
+      <button id='protease' className='button' onClick={() => onEnzymeAdd('protease', 'proteins')}>
+        Add Protease
+      </button>
+      <button id='lipase' className='button' onClick={() => onEnzymeAdd('lipase', 'lipids')}>
+        Add Lipase
       </button>
     </>
   );
